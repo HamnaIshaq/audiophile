9d06c64cf7b29b4a02137b5b9fd9bef3454401c2
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-07-21 23:34:35 +0500

    Initial Commit

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,5 @@
+const ProductCard = () => {
+  return <section>product card section</section>;
+};
+
+export default ProductCard;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ProductCard from "./components/ProductCard";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ProductCard />
+  </React.StrictMode>
+);
