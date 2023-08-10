ab703ead41c23333f2571800ce9e05dee1ff0f56
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-08-11 00:17:12 +0500

    Product card component to show categories of products

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,32 @@
+import { productCategories } from "../data/data";
+import { data } from "../data/data";
+
 const ProductCard = () => {
-  return <section>product card section</section>;
+  const onClickProductCategory = (category) => {
+    console.log(category);
+    const productsOfSelectedCategory = data.filter(
+      (products) => products.category === category
+    );
+    console.log(productsOfSelectedCategory);
+  };
+
+  return (
+    <section>
+      {/*<a href="#">
+        <img src="" alt="img" />
+      </a>*/}
+      {productCategories.map((category) => {
+        return (
+          <div
+            key={category.id}
+            onClick={() => onClickProductCategory(category.name)}
+          >
+            {category.name}
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 
 export default ProductCard;
