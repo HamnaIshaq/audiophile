722b9bb53b5ef33b34a8e644b4db7ddb7099d63e
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-08-11 00:17:24 +0500

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./assets/css/normalize.css";
+import "./assets/css/index.css";
+//import Header from "./components/Header";
 import ProductCard from "./components/ProductCard";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ProductCard />
