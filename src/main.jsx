5cf16d1218017b93fc89aa8892fe4dafba1fef4c
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-08-14 23:53:34 +0500

    Show Checkout page

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/normalize.css";
 import "./assets/css/index.css";
+import Checkout from "./pages/Checkout";
 //import Header from "./components/Header";
-import ProductCard from "./components/ProductCard";
+//import ProductCard from "./components/ProductCard";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ProductCard />
+    <Checkout />
   </React.StrictMode>
 );
