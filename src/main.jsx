237027041d2fb35f4a84105eec8e1a5c8638a6c0
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-08-27 22:12:20 +0500

    Show App component

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/normalize.css";
 import "./assets/css/index.css";
-import Checkout from "./pages/Checkout";
+//import Checkout from "./pages/Checkout";
+import App from "./App";
 //import Header from "./components/Header";
 //import ProductCard from "./components/ProductCard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Checkout />
+    <App />
   </React.StrictMode>
 );
